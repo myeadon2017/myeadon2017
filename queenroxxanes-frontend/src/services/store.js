3872220d925815f0f6edd109912ab4558781a817
myeadon2017 <62955849+myeadon2017@users.noreply.github.com>
2020-08-23 20:25:27 -0400

    store is done

---
+++ queenroxxanes-frontend/src/services/store.js
@@ -0,0 +1,61 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    user: null, //username and password?
+    username: '',
+    password: '',
+    userList: [],
+    currentAppointments: [],
+    appointments: [],
+    appointment: {'client_id': -1, 'appointment_type': '', 'appointment_date': ''},
+    appointmentList: [],
+    client: {},
+    manager: {}
+}
+
+
+function queenRoxxaneReducer(state = initialState, action){
+    switch(action.type){
+        case 'login':
+            return Object.assign({}, state, {username: '', user: action.user})
+        case 'handleUsername':
+            return Object.assign({}, state, {username: action.username})
+        case 'handlePassword':
+            return Object.assign({}, state, {password: action.password})
+        case 'handleAppointmentChange':
+            return Object.assign({}, state, {appointment: action.appointment})
+        case 'addAppointment':
+            return Object.assign({}, state, {appointment: action.appointment})
+        case 'loadAppointmentList':
+            return Object.assign({}, state, {appointmentList: action.appointmentList})
+        case 'loadUserList':
+            return Object.assign({}, state, {userList: action.userList})
+        case 'loadAppointment':
+            return Object.assign({}, state, {appointment: action.appointment})
+        case 'loadUser':
+            return Object.assign({}, state, {user: action.user})
+        case 'removeUser':
+            return Object.assign({}, state, {userList: action.userList})
+        case 'handleUsernameChange':
+            return Object.assign({}, state, {client: action.client})
+        case 'handlePasswordChange':
+            return Object.assign({}, state, {client: action.client})
+        case 'handleUserFieldChange':
+            return Object.assign({}, state, {user: action.user})   
+        case 'loadCurrentAppointments':
+            return Object.assign({}, state, {currentAppointments: action.currentAppointments})
+        case 'handleManUsernameChange':
+            return Object.assign({}, state, {manager: action.manager})
+        case 'handleManPasswordChange':
+            return Object.assign({}, state, {manager: action.manager})
+        default:
+            return state;
+    }
+}
+
+
+
+
+let store = createStore(queenRoxxaneReducer);
+
+export default store;
